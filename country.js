f4e129dc8fc066b95407685cea143e9991453d58
meetsethiya16 <meet.sethiya.16@gmail.com>
2024-06-25 06:36:48 +0530

    individual country page added

--- country.js
+++ country.js
@@ -1,8 +1,52 @@
 const countryName = new URLSearchParams(location.search).get("name");
+const flagImg = document.querySelector(".country-details img");
+const countryNameH1 = document.querySelector(".country-details h1");
+const nativeName = document.querySelector(".native-name");
+const population = document.querySelector(".population");
+const region = document.querySelector(".region");
+const subRegion = document.querySelector(".sub-region");
+const capital = document.querySelector(".capital");
+const topLevelDomain = document.querySelector(".top-level-domain");
+const currencies = document.querySelector(".currencies");
+const languages = document.querySelector(".languages");
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=True`).then(
   (res) =>
-    res.json().then((data) => {
-      console.log(data[0]);
+    res.json().then(([country]) => {
+      console.log(country);
+      flagImg.src = country.flags.svg;
+      countryNameH1.innerText = country.name.common;
+
+      if (country.name.nativeName) {
+        nativeName.innerText = Object.values(country.name.nativeName)[0].common;
+      } else {
+        nativeName.innerText = country.name.common;
+      }
+
+      population.innerText = new Intl.NumberFormat("en-IN", {
+        maximumSignificantDigits: 3,
+      }).format(country.population);
+
+      region.innerText = country.region;
+
+      if (country.subRegion) {
+        subRegion.innerText = country.subregion;
+      }
+
+      if (country.capital) {
+        capital.innerText = country.capital?.[0];
+      }
+
+      topLevelDomain.innerText = country.tld.join(", .");
+
+      if (country.currencies) {
+        currencies.innerText = Object.values(country.currencies)
+          .map((currency) => currency.name)
+          .join(", ");
+      }
+
+      if (country.languages) {
+        languages.innerText = Object.values(country.languages).join(", ");
+      }
     })
 );
--- script.js
+++ script.js
@@ -4,14 +4,16 @@ fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((country) => {
+      console.log(country.name.common);
+      console.log(country.borders);
       const countryCard = document.createElement("a");
-      //   console.log(countryCard);
       countryCard.href = `/country.html?name=${country.name.common}`;
-      //   countryCard.target = "_blank";
       countryCard.classList.add("country-card");
 
       countryCard.innerHTML = `
-                    <img src="${country.flags.svg}" alt="flag">
+                    <img src="${
+                      country.flags.svg
+                    }" alt="{country.name.common} flag">
                       <div class="card-text">
                           <h3 class="card-title">${country.name.common}</h3>
                           <p><b>Population: </b>${new Intl.NumberFormat(
